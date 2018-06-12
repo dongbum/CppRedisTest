440f8b6dbde0a0d920b166e81c4b37fa197f0f3e
DONGBUM, KIM <dongbum9@gmail.com>
2018-06-12 19:00:51 +0900

    입력코드 추가

--- CppRedisTest/main.cpp
+++ CppRedisTest/main.cpp
@@ -3,32 +3,65 @@
 
 #include "RedisWrapper.h"
 
+std::vector<std::string> split(std::string target, std::string delimiter) {
+	std::vector<std::string> result;
+	int position = target.find(delimiter);
+
+	while (position != -1) {
+		std::string str1 = target.substr(0, position);
+		result.push_back(str1);
+		target = target.substr(position + 1);
+		position = target.find(delimiter);
+	}
+	result.push_back(target);
+
+	return result;
+}
+
+RedisWrapper redis;
+
+void InputFunction(void)
+{
+	while (true)
+	{
+		std::string input_msg = "";
+
+		std::cout << "Input : ";
+		std::getline(std::cin, input_msg);
+
+		std::cout << "InputMsg : " << input_msg << std::endl;
+
+		std::vector<std::string> command = split(input_msg, " ");
+
+		for (size_t i = 0; i < command.size(); ++i)
+		{
+			std::cout << i << ":" << command[i] << std::endl;
+		}
+
+		std::string result;
+
+		if (command[0] == "get")
+		{
+			redis.Get(command[1], result);
+			std::cout << "GET Result : " << result << std::endl;
+		}
+	};
+}
+
 int main(void)
 {
 	INIT_CPP_REDIS();
 
-	static auto InputFunction = []()
-	{
-		while (true)
-		{
-			std::string input_msg = "";
-			std::getline(std::cin, input_msg);
-
-			std::cout << "InputMsg : " << input_msg << std::endl;
-		};
-	};
-
-	std::thread input_thread = std::thread(InputFunction);
-
-	RedisWrapper redis;
+	
 	redis.Init();
 	if (false == redis.Connect("127.0.0.1"))
 		return -1;
 
-	redis.DisConnect();
-	
+	std::thread input_thread = std::thread(InputFunction);
 	input_thread.join();
 
+	redis.DisConnect();
+	
 	CLEAR_CPP_REDIS();
 	
 	return 0;
